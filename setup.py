213f1f3a9aa5bcc38ba54a625ec512d19e9f5507
chris104957 <christopherdavies553@gmail.com>
2017-10-20 15:48:50 +0100

    Bumping version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-carrot',
-    version='0.2.0',
+    version='0.1.1',
     packages=find_packages(),
     include_package_data=True,
     license='Apache Software License',
