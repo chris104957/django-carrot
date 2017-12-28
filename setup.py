2d940e57205548faacf65678c5d16406ad8f2422
Chris Davies <christopher.davies@asg-worldwide.com>
2017-12-28 16:03:01 +0000

    removing support for python 3.4

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Topic :: Internet :: WWW/HTTP',
