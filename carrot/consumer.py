c3fdfe6385996d1ff89d05f5e856d9ab2a97d730
Chris Davies <christopher.davies@asg-worldwide.com>
2018-01-03 08:02:22 +0000

    reverting log saving method to fix #32

--- carrot/consumer.py
+++ carrot/consumer.py
@@ -344,21 +344,23 @@ class Consumer(threading.Thread):
 
             log.log = '\n'.join(self.task_log)
 
-            while self.remaining_save_attempts > 0:
-                self.remaining_save_attempts -= 1
+            save_attempts = 0
+
+            while True:
+                save_attempts += 1
 
                 try:
                     log.save()
                     self.active_message_log = None
 
-                    return
+                    break
                 except OperationalError:
+                    if save_attempts > 10:
+                        raise OperationalError('Unable to access the database. This is probably because the number '
+                                               'of carrot threads is too high. Either reduce the amount of '
+                                               'scheduled tasks consumers, or increase the max number of '
+                                               'connections supported by your database')
                     self.connection.sleep(10)
-
-            raise OperationalError('Unable to access the database. This is probably because the number '
-                                   'of carrot threads is too high. Either reduce the amount of '
-                                   'scheduled tasks consumers, or increase the max number of '
-                                   'connections supported by your database')
 
         except Exception as err:
             self.task_log.append(task.get_logs())
