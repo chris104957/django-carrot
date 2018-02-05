0eeaf57ef16caa53bd2875bc804d122f0fcd7a56
chris104957 <christopherdavies553@gmail.com>
2018-02-05 09:41:50 +0000

    Update conf.py
    redirecting to new landing page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ source_suffix = '.rst'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'quick-start'
+master_doc = 'index'
 
 # General information about the project.
 project = 'django-carrot'
